8d34cf40e07d5279b8f88ee855c363368c291cbd
Raphael Mansueto <raphaelmansueto@gmail.com>
2024-10-18 04:53:24 +0800

    fix: resolved incorrect verbatim

--- src/app/(landing-page)/_components/hero-section.tsx
+++ src/app/(landing-page)/_components/hero-section.tsx
@@ -9,7 +9,7 @@ const heroContent = {
   description:
     'Passionate Full-Stack Developer focused on Front-End Engineering. Experienced in React, Next.js, and AI integration, I build scalable web applications and deliver seamless user experiences across industries.',
   tagline:
-    'Intelligence-driven, creating reliable solutions focused on scalability and exceptional quality..',
+    'Intelligence-driven, creating reliable solutions focused on scalability and exceptional quality.',
   ctaText: 'Explore My Work',
 }
 
