1852619607c44ab6663626faec3f4d4a5238f482
Raphael Mansueto <raphaelmansueto@gmail.com>
2024-10-29 05:30:48 +0800

    fix: migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241028_195515_initial from './20241028_195515_initial';
+import * as migration_20241028_213035_blog_html from './20241028_213035_blog_html';
 
 export const migrations = [
   {
     up: migration_20241028_195515_initial.up,
     down: migration_20241028_195515_initial.down,
-    name: '20241028_195515_initial'
+    name: '20241028_195515_initial',
+  },
+  {
+    up: migration_20241028_213035_blog_html.up,
+    down: migration_20241028_213035_blog_html.down,
+    name: '20241028_213035_blog_html'
   },
 ];
