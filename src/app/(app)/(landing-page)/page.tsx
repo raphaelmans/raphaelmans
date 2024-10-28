61943ce7fa0fdce1c3036212480317600f335b82
Raphael Mansueto <raphaelmansueto@gmail.com>
2024-10-29 05:23:37 +0800

    fix: use direct call instead to database

--- src/app/(app)/(landing-page)/page.tsx
+++ src/app/(app)/(landing-page)/page.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import type { PaginatedDocs } from 'payload'
+import { getPayload } from 'payload'
 import Hero from './_components/hero-section'
 import BlogSection from './_components/blogs-section'
 import ProjectsSection from './_components/projects-section'
 import { APIBaseResponse } from '@/lib/schemas'
 import { Blog } from '@/payload-types'
-import { API_BASE_URL } from '@/common/constants'
+import configPromise from '@/payload.config'
+
+export const revalidate = 3600 // Revalidate every hour
 
 export default async function LandingPage() {
-  const res = await fetch(API_BASE_URL + '/blogs')
+  const payload = await getPayload({
+    config: configPromise,
+  })
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
+  const blogs = await payload.find({
+    collection: 'blog',
+  })
+
+  const data: APIBaseResponse<PaginatedDocs<Blog>> = {
+    data: blogs,
   }
 
-  const data: APIBaseResponse<PaginatedDocs<Blog>> = await res.json()
   return (
     <React.Fragment>
       <Hero />
