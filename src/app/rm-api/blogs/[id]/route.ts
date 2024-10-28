e9492ab2f6d71887ad141ce90864061f01ce3d41
Raphael Mansueto <raphaelmansueto@gmail.com>
2024-10-29 05:00:49 +0800

    build: changes in relation to v15 api

--- src/app/rm-api/blogs/[id]/route.ts
+++ src/app/rm-api/blogs/[id]/route.ts
@@ -2,14 +2,20 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
 
-export const GET = async (_request: NextRequest, { params }: { params: { id: string } }) => {
+export const GET = async (
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
   const payload = await getPayload({
     config: configPromise,
   })
+
+  const { id } = await params
+
   const blog = await payload.find({
     collection: 'blog',
     where: {
-      id: { equals: params.id },
+      id: { equals: id },
     },
   })
   return NextResponse.json({ data: blog })
